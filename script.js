fe1e413a59eb44eae0ec9f7ada4b4f2a2a58fa1a
finnian-smith <finniansmith01@gmail.com>
2024-01-29 13:09:47 +0000

    Style game and gameplay

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const GRID_MIN_LIMIT = 0;
 const GRID_MAX_LIMIT = 100;
 
+let initialGridSize = 16;
+
 const container = document.querySelector(".grid-container");
 
 // Function to create a grid with a specified number of squares per side
@@ -19,13 +21,15 @@ function createGrid(squarePerSide) {
 
   container.addEventListener("mouseover", function (event) {
     if (event.target.classList.contains("grid-square")) {
-      event.target.style.backgroundColor = "#66ccff";
+      const backgroundColor = randomColor();
+      event.target.style.backgroundColor = `rgb(${backgroundColor})`;
     }
   });
 }
 
 // Initial grid creation
-createGrid(16);
+createGrid(initialGridSize);
+displayGridSize(initialGridSize);
 
 // Resize grid button event listener
 const button = document.querySelector(".resize-button");
@@ -40,5 +44,26 @@ button.addEventListener("click", function () {
     alert("Please enter a valid number between 1 and 100.");
   } else {
     createGrid(newSize);
+    displayGridSize(newSize);
   }
 });
+
+// each interaction randomises the square’s RGB value
+function randomColor() {
+  const rgbValue = Array.from({ length: 3 }, () =>
+    Math.floor(Math.random() * 266)
+  );
+
+  return rgbValue;
+}
+
+// display current grid size
+function displayGridSize(gridSize) {
+  const gridSizeElement = document.querySelector(".grid-size");
+  gridSizeElement.textContent = gridSize + "x" + gridSize;
+}
+
+// Add button to index.html -> show grid or not
+// Add event listener to button -> toggle on / off = border: 1px solid #ffffff
+// will probably need to be move styling here and then handle
+// but initially set show grid = false?
